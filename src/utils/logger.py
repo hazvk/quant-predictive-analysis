0b61680c5d9c878f9472ca0bdfbfe46441981460
Hari V <hariharen.veerakumar@gmail.com>
2025-12-26 21:01:47 +1100

    Bugs in logging,done properly now

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,11 +2,13 @@ import logging
 import os
 from datetime import datetime
 from pathlib import Path
+from dotenv import load_dotenv
 
 class Logger:
     def __init__(self):
+        load_dotenv("config/.env")
         self.name = "quant-predictive-analysis"
-        self.log_dir = Path(os.environ["QUANT_PRED_ANALYSIS_DUCKDB_STORAGE_PATH"] / "logs")
+        self.log_dir = Path(os.environ["QUANT_PRED_ANALYSIS_DUCKDB_STORAGE_PATH"]) / "logs"
         self.logger = self._setup_logger()
 
     def _setup_logger(self) -> logging.Logger:
@@ -14,28 +16,30 @@ class Logger:
         os.makedirs(self.log_dir, exist_ok=True)
 
         logger = logging.getLogger(self.name)
-        logger.setLevel(logging.DEBUG)
 
-        # File handler
-        log_file = os.path.join(
-            self.log_dir, f"{self.name}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
-        )
-        file_handler = logging.FileHandler(log_file)
-        file_handler.setLevel(logging.DEBUG)
+        if not logger.hasHandlers():
+            logger.setLevel(logging.DEBUG)
 
-        # Console handler
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
+            # File handler
+            log_file = os.path.join(
+                self.log_dir, f"{self.name}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+            )
+            file_handler = logging.FileHandler(log_file)
+            file_handler.setLevel(logging.DEBUG)
 
-        # Formatter
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
-        file_handler.setFormatter(formatter)
-        console_handler.setFormatter(formatter)
+            # Console handler
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(logging.INFO)
 
-        logger.addHandler(file_handler)
-        logger.addHandler(console_handler)
+            # Formatter
+            formatter = logging.Formatter(
+                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            )
+            file_handler.setFormatter(formatter)
+            console_handler.setFormatter(formatter)
+
+            logger.addHandler(file_handler)
+            logger.addHandler(console_handler)
 
         return logger
 
