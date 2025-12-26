694830ae2ad4411a33ec1dcaa05d0793961c4904
Hari V <hariharen.veerakumar@gmail.com>
2025-12-26 17:26:33 +1100

    Main split into ingest, we can extend later with other functions

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from dotenv import load_dotenv
 from src.ingestion.specified_tickers.nvda_ingestor import NvdaIngestor
 
 
+load_dotenv()
 
-def main():
-    load_dotenv()
+def _ingest_data():
     NvdaIngestor().ingest_stock_data("2025-11-20", "2025-12-21")
 
+def main():
+    _ingest_data()
 
 if __name__ == "__main__":
     main()
